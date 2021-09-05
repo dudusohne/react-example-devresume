51c25e9b48c2fa9ae9dea87e418c3d8645d2fe01
Eduardo Sohne <dudu.hit@gmail.com>
2021-09-05 16:36:01 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { Route, BrowserRouter } from 'react-router-dom';
 import { AuthContextProvider } from './context/AuthContext';
 import { AuthPage } from './pages/AuthPage';
 import { Home } from './pages/Home';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 function App() {
   return (
